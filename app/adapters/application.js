a06493a7f0e1eb60917266997cb71f7a189e4c61
sollabeck <kevin.ceballos@gmail.com>
2016-05-24 16:58:33 -0700

    change host location

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-	host: 'http://127.0.0.1:3000'
+	host: 'https://kevin-backend.herokuapp.com'
 
 });
 
